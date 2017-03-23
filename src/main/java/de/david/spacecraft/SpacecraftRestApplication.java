f5263e852f71c66142bfce706d8970457816b300
David Klassen <daviddd.kl@gmail.com>
2017-03-23 08:45:49 +0100

    Add initial classes

---
+++ src/main/java/de/david/spacecraft/SpacecraftRestApplication.java
@@ -0,0 +1,12 @@
+package de.david.spacecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacecraftRestApplication.class, args);
+	}
+}
