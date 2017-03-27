7b1212d37acd40db0af552aef310edb44c417cc1
David Klassen <daviddd.kl@gmail.com>
2017-03-27 10:18:20 +0200

    Add tests for new services

--- src/main/java/de/david/spacecraft/rest/SpacecraftRestController.java
+++ src/main/java/de/david/spacecraft/rest/SpacecraftRestController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * @author David Klassen
